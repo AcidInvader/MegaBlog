f8b228557c4949104d39b8496fc8a1f41ef57d1a
AcidInvader <124760286+AcidInvader@users.noreply.github.com>
2023-08-07 17:46:21 +0300

    detail page is done

--- web/blog/static/blog/js/detail.js
+++ web/blog/static/blog/js/detail.js
@@ -1,6 +1,21 @@
 $(function () {
-
+    const getParams = getQueryParams()
+    let id = getParams.id
+    $.ajax({
+        url: `/api/v1/article/blog/${id}`,
+    }).then(function (data) {
+        console.log(data)
+        successHandler(data)
+    })
 
 });
 
+function successHandler(data) {
+    $('h1').html(data.title);
+    $('.fa-user').html(data.author.full_name);
+    $('.fa-calendar').html(data.updated);
+    // $('.img-responsive').attr('src', `${data.image}`);
+    $('#content').html(data.content);
+    
+};
 console.log('blog-detail')
